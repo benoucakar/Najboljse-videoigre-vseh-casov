bde35a385eb55ad7e3c8c07552f4fcd48b7b9fca
Beno Učakar <beno.ucakar@gmail.com>
2020-10-28 23:15:17 +0100

    Dodal funkcijo, ki podatke s strani da v JSON. Poleg tega je tu posodobljeno številčenje strani, ki še noso bile v prejšnjem commitu.

--- pobiranje_url.py
+++ pobiranje_url.py
@@ -2,6 +2,8 @@ import orodja
 import re
 
 bi_res_prenesel_strani = False
+bi_res_poiskal_podatke = False
+
 stevilo_strani = 100
 json_datoteka = "url.json"
 mapa_podatkov = "zajeti_podatki_url"
@@ -9,7 +11,7 @@ mapa_podatkov = "zajeti_podatki_url"
 def prenos_strani(bi_res_prenesel_strani, stevilo_strani):
     if bi_res_prenesel_strani:
         for i in range(stevilo_strani):
-            orodja.url_v_html(f"https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?view=detailed&page={i}", mapa_podatkov, f"igre_od_{i * 100 + 1}_do_{(i + 1) * 100}.html")
+            orodja.url_v_html(f"https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?view=detailed&page={i}", mapa_podatkov, f"igre_od_{i * 100}_do_{i * 100 + 99}.html")
         print("Končano!")
 
 def bloki_iz_strani(stran):
@@ -24,11 +26,14 @@ def pridobivanje_podatkov_s_strani(mapa_podatkov, stran):
     seznam_blokov = bloki_iz_strani(orodja.vsebina_datoteke(mapa_podatkov, stran))
     return [slovar_iz_bloka(blok) for blok in seznam_blokov]
 
-# print(blok_iz_strani(orodja.vsebina_datoteke(mapa_podatkov, "test.html"))[0])
-# print(slovar_iz_bloka(blok_iz_strani(orodja.vsebina_datoteke(mapa_podatkov, "test.html"))[1]))
+def podatki_v_json(bi_res_poiskal_podatke, stevilo_strani, mapa_podatkov, ime_json_datoteke):
+    if bi_res_poiskal_podatke:
+        seznam_podatkov = []
+        for i in range(stevilo_strani):
+            podatki = pridobivanje_podatkov_s_strani(mapa_podatkov, f"igre_od_{i * 100}_do_{i * 100 + 99}.html")
+            seznam_podatkov += podatki
+        orodja.zapisi_json(seznam_podatkov, ime_json_datoteke)
 
 prenos_strani(bi_res_prenesel_strani, stevilo_strani)
 
-# orodja.zapisi_json([1,2,3], "", json_datoteka)
-
-print(pridobivanje_podatkov_s_strani(mapa_podatkov, "igre_od_0_do_99.html") * 100)
+podatki_v_json(bi_res_poiskal_podatke, 100, mapa_podatkov, json_datoteka)
