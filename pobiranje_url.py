b47e04414697f7d05835bac8cc53e0a845be7a8a
Beno Učakar <beno.ucakar@gmail.com>
2020-10-28 21:04:46 +0100

    Dodal funkcijo za prenos strani. Potem sem tudi prenesel strani.

--- pobiranje_url.py
+++ pobiranje_url.py
@@ -1,12 +1,16 @@
 import orodja
 import re
 
+bi_res_prenesel_strani = False
+stevilo_strani = 100
 json_datoteka = "url.json"
 mapa_podatkov = "zajeti_podatki_url"
 
-test_url = "https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?view=detailed&page=1"
-
-# orodja.url_v_html(test_url, mapa_podatkov, "test.html")
+def prenos_strani(bi_res_prenesel_strani, stevilo_strani):
+    if bi_res_prenesel_strani:
+        for i in range(stevilo_strani):
+            orodja.url_v_html(f"https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?view=detailed&page={i}", mapa_podatkov, f"igre_od_{i * 100 + 1}_do_{(i + 1) * 100}.html")
+        print("Končano!")
 
 def blok_iz_strani(stran):
     vzorec = re.compile(r'<input type="checkbox".*?</td>', re.DOTALL)
@@ -18,3 +22,5 @@ def slovar_iz_bloka(blok):
 
 # print(blok_iz_strani(orodja.vsebina_datoteke(mapa_podatkov, "test.html"))[0])
 # print(slovar_iz_bloka(blok_iz_strani(orodja.vsebina_datoteke(mapa_podatkov, "test.html"))[1]))
+
+prenos_strani(bi_res_prenesel_strani, stevilo_strani)
