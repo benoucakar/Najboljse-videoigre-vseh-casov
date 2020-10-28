645feb9ca14b18c5456217e12b88c6affe378ce4
Beno Učakar <beno.ucakar@gmail.com>
2020-10-28 16:52:17 +0100

    Dodelal orodja.py - Spisal svoj program za snemanje stani s spleta, ki podpira nastavitev headerjev in destinacijo za shranjevanje - Citiral vire - Zakomentiral program in funkcije razdelil v kategorije.

--- orodja.py
+++ orodja.py
@@ -1,55 +1,51 @@
+# Prirejeno po datotekah iz predavanj in vaj.
+
 import csv
 import json
 import os
 import requests
-import sys
 
 default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
+# Prenos spletne strani
 
-def pripravi_imenik(ime_datoteke):
-    '''Če še ne obstaja, pripravi prazen imenik za dano datoteko.'''
-    imenik = os.path.dirname(ime_datoteke)
-    if imenik:
-        os.makedirs(imenik, exist_ok=True)
-
-
-def shrani_spletno_stran(url, ime_datoteke, vsili_prenos=False, headers=default_headers):
-    '''Vsebino strani na danem naslovu shrani v datoteko z danim imenom.'''
+def url_v_html(url, mapa, ime_datoteke, headers=default_headers):
+    '''Sprejme url in v dano destinacijo shrani HTML datoteko.'''
     try:
-        print(f'Shranjujem {url} ...', end='')
-        sys.stdout.flush()
-        if os.path.isfile(ime_datoteke) and not vsili_prenos:
-            print('shranjeno že od prej!')
-            return
-        r = requests.get(url, headers=headers)
+        page_content = requests.get(url, headers=headers) 
     except requests.exceptions.ConnectionError:
-        print('stran ne obstaja!')
+        print(f"Napaka pri povezovanju na {url}")
+        return None
+    if page_content.status_code == requests.codes.ok:
+        os.makedirs(mapa, exist_ok=True)
+        path = os.path.join(mapa, ime_datoteke)
+        with open(path, 'w', encoding='utf-8') as file_out:
+            file_out.write(page_content.text)
     else:
-        pripravi_imenik(ime_datoteke)
-        with open(ime_datoteke, 'w', encoding='utf-8') as datoteka:
-            datoteka.write(r.text)
-            print('shranjeno!')
+        print(f"Napaka pri prenosu strani {url}")
+        return None
 
+# Analiza spletne strani
 
-def vsebina_datoteke(ime_datoteke):
-    '''Vrne niz z vsebino datoteke z danim imenom.'''
-    with open(ime_datoteke, encoding='utf-8') as datoteka:
+def vsebina_datoteke(mapa, ime_datoteke):
+    '''Vrne niz z vsebino dane datoteke.'''
+    with open(os.path.join(mapa, ime_datoteke), encoding='utf-8') as datoteka:
         return datoteka.read()
 
+# Shranjevanje
 
-def zapisi_csv(slovarji, imena_polj, ime_datoteke):
+def zapisi_csv(slovarji, imena_polj, mapa, ime_datoteke):
     '''Iz seznama slovarjev ustvari CSV datoteko z glavo.'''
-    pripravi_imenik(ime_datoteke)
-    with open(ime_datoteke, 'w', encoding='utf-8') as csv_datoteka:
+    os.makedirs("mapa", exist_ok=True)
+    with open(os.path.join(mapa, ime_datoteke), 'w', encoding='utf-8') as csv_datoteka:
         writer = csv.DictWriter(csv_datoteka, fieldnames=imena_polj)
         writer.writeheader()
         for slovar in slovarji:
             writer.writerow(slovar)
 
 
-def zapisi_json(objekt, ime_datoteke):
+def zapisi_json(objekt, mapa, ime_datoteke):
     '''Iz danega objekta ustvari JSON datoteko.'''
-    pripravi_imenik(ime_datoteke)
-    with open(ime_datoteke, 'w', encoding='utf-8') as json_datoteka:
+    os.makedirs("mapa", exist_ok=True)
+    with open(os.path.join(mapa, ime_datoteke), 'w', encoding='utf-8') as json_datoteka:
         json.dump(objekt, json_datoteka, indent=4, ensure_ascii=False)
