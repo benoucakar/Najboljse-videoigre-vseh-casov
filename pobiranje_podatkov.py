45139e0d3e86c26edd2e0937af58aae97ff52277
Beno Učakar <beno.ucakar@gmail.com>
2020-10-29 17:38:00 +0100

    Dodal jedro_iz_strani(), ki vrne bistven del HTML datoteke, kjer se nahajajo podatki.

--- pobiranje_podatkov.py
+++ pobiranje_podatkov.py
@@ -13,4 +13,13 @@ def prenos_strani(id, url_rep, mapa):
     url = 'https://www.metacritic.com' + url_rep
     orodja.url_v_html(url, mapa, f"{id}.html")
 
-# prenos_strani("160692", "/game/nintendo-64/the-legend-of-zelda-ocarina-of-time", mapa_podatkov)
+def jedro_iz_strani(stran):
+    '''Vrne le del strani, kjer se nahajajo podatki.'''
+    vzorec = re.compile(r'<h1>.*?</div>\s+</div>\s+</div>\s+</div>\s+<div class="summary_trailer">', re.DOTALL)
+    return re.search(vzorec, stran).group(0)
+
+# <h1>(?P<naslov>(.*?))</h1>.*?<span class="platform">\s*(?P<platforma>(.*?))\n.*?<a href.*?>\s*?(?P<razvijalec>(\w*?))\s*    
+
+# prenos_strani("108362", "/game/playstation-3/grand-theft-auto-iv", mapa_podatkov)
+
+# print(jedro_iz_strani(orodja.vsebina_datoteke(mapa_podatkov, "108362.html")))
