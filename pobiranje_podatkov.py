897d98cb8a948e13a3a9d9835286da4db4d0cfb0
Beno Učakar <beno.ucakar@gmail.com>
2020-11-01 20:28:48 +0100

    Dodal vzorce za posamezne podatke na strani.

--- pobiranje_podatkov.py
+++ pobiranje_podatkov.py
@@ -9,6 +9,66 @@ mapa_podatkov = "zajeti_podatki_igre"
 
 # Definiramo funkcije
 
+vzorec_naslov = re.compile(
+    r'<h1>(?P<naslov>(.*?))</h1>'
+    flags=re.DOTALL
+)
+
+vzorec_platforma = re.compile(
+    r'<span class="platform">\s+?<a href="[\w/-]*">\s+(?P<platforma>(\S.*?))\s+?</a>'
+    flags=re.DOTALL
+)
+
+vzorec_studio = re.compile(
+    r'<a href="[\w/-]*?"  >\s+?(?P<studio>(\S.*?))\s*?</a>'
+    flags=re.DOTALL
+)
+
+vzorec_mesec_in_leto = re.compile(
+    r'<span class="data" >((?P<mesec>)(\w{3})) \d{2}, (?P<leto>(\d{4}))</span>'
+    flags=re.DOTALL
+)
+
+vzorec_metascore = re.compile(
+    r'<span>(?P<metascore>(\d+))</span></div>'
+    flags=re.DOTALL
+)
+
+vzorec_st_glasov_metascore = re.compile(
+    r'>\s+?(?P<st_glasov_metascore>(\d+))\s+?</span> Critic Reviews'
+    flags=re.DOTALL
+)
+
+vzorec_userscore = re.compile(
+    r'<div class="metascore_w user large game positive">(?P<userscore>(\d\.?\d))</div>'
+    flags=re.DOTALL
+)
+
+vzorec_st_glasov_userscore = re.compile(
+    r'/user-reviews">(?P<st_glasov_userscore>(\d+?)) Ratings'
+    flags=re.DOTALL
+)
+
+vzorec_opis = re.compile(
+    r'<span class="blurb blurb_expanded">(?P<opis>(.*?))</span>'
+    flags=re.DOTALL
+)
+
+vzorec_zanri = re.compile(
+    r'<span class="label">Genre\(s\): </span>(?P<zanri>(.*?))</li>'
+    flags=re.DOTALL
+)
+
+vzorec_oznaka = re.compile(
+    r'<span class="label">Rating:</span>.*?>(?P<oznaka>(.*?))</span>'
+    flags=re.DOTALL
+)
+
+vzorec_st_igralcev = re.compile(
+    r'<span class="label"># of players:</span>.*?>(?P<st_igralcev>(.*?))</span>'
+    flags=re.DOTALL
+)
+
 class Igra():
     def __init__(self):
         self.id = 0
