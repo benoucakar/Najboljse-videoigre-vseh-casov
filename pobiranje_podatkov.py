4de60e304683fb6af4a9270f2b1210c4cfb7bd33
Beno Učakar <beno.ucakar@gmail.com>
2020-11-02 00:06:46 +0100

    Odstranil razred Igra

--- pobiranje_podatkov.py
+++ pobiranje_podatkov.py
@@ -79,23 +79,6 @@ vzorec_st_igralcev = re.compile(
     flags=re.DOTALL
 )
 
-class Igra():
-    def __init__(self):
-        self.id = 0
-        self.naslov = ""
-        self.platforma = ""
-        self.studio = ""
-        self.mesec_izida = ""
-        self.leto_izida = ""
-        self.metascore = 0
-        self.st_glasov_metascore = 0
-        self.userscore = 0
-        self.st_glasov_userscore = 0
-        self.opis = ""
-        self.zanri = []
-        self.oznaka = ""
-        self.st_igralcev = ""
-
 def prenos_strani(id, url_rep, mapa):
     url = 'https://www.metacritic.com' + url_rep
     orodja.url_v_html(url, mapa, f"{id}.html")
