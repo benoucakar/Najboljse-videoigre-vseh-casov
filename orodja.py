2b85186ef5686b043d766da14c0a747a8df392cb
Beno Učakar <beno.ucakar@gmail.com>
2020-11-02 00:13:05 +0100

    Malo preuredil rapored, da je bolj pregledno.

--- orodja.py
+++ orodja.py
@@ -7,7 +7,9 @@ import requests
 
 default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
+
 # Prenos spletne strani
+
 
 def url_v_html(url, mapa, ime_datoteke, headers=default_headers):
     '''Sprejme url in v dano destinacijo shrani HTML datoteko.'''
@@ -25,14 +27,15 @@ def url_v_html(url, mapa, ime_datoteke, headers=default_headers):
         print(f"Napaka pri prenosu strani {url}")
         return None
 
-# Analiza spletne strani
+
+# Pisanje in odpiranje
+
 
 def vsebina_datoteke(mapa, ime_datoteke):
     '''Vrne niz z vsebino dane datoteke.'''
     with open(os.path.join(mapa, ime_datoteke), encoding='utf-8') as datoteka:
         return datoteka.read()
 
-# Pisanje in odpiranje
 
 def zapisi_csv(slovarji, imena_polj, ime_datoteke):
     '''Iz seznama slovarjev ustvari CSV datoteko z glavo.'''
@@ -47,6 +50,7 @@ def zapisi_json(objekt, ime_datoteke):
     '''Iz danega objekta ustvari JSON datoteko.'''
     with open(ime_datoteke, 'w', encoding='utf-8') as json_datoteka:
         json.dump(objekt, json_datoteka, indent=4, ensure_ascii=False)
+
 
 def odpri_json(ime_datoteke):
     '''Odpre dano JSON datoteko.'''
